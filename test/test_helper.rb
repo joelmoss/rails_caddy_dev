baaa7ea1d3853be8ab9a55c7a9debbe7594bdc17
Joel Moss <joel@developwithstyle.com>
2026-02-10 15:57:37 +0000

    fix: rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "rails_caddy_dev"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'rails_caddy_dev'
 
-require "minitest/autorun"
+require 'minitest/autorun'
